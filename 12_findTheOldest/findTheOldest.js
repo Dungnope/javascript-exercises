7325bcb747dc3d49928ed58bc7cbadc084c2b409
Dungnope <140576667+Dungnope@users.noreply.github.com>
2025-04-23 16:44:24 +0700

    The final exercise used sort and reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const d = new Date();
+    let year = d.getFullYear();
+    let agelist = new Array(people.length);
+    let ages = people.reduce((acc, curr, index) => {
+        if(curr.yearOfDeath === undefined) curr.yearOfDeath = year;
+        curr.age = curr.yearOfDeath - curr.yearOfBirth;
+        acc[index] = curr;
+        agelist.push(curr.age);
+        return acc;
+    }, [])
+    let oldestage = ages.sort((a, b) => {
+        return a.age - b.age;
+    })
+    return oldestage[oldestage.length - 1];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
