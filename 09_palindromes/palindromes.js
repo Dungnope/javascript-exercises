4963ee2b1fbfd07853e82ecd6b64ff2f5872573a
Dungnope <140576667+Dungnope@users.noreply.github.com>
2025-04-23 13:51:20 +0700

    2 ways from me and solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+   //   const newstring = string.toLowerCase().split("");
+   //   const ans = newstring.map(value => {
+   //      if(value === ' ' || value === ',' || value === '!' || value === '.') return value = '';
+   //      else return value;
+   //   })
+   //   let compare1 = ans.join("");
+   //   ans.reverse();
+   //   let compare2 = ans.join("");
+   //   return compare1 === compare2;
+   /* Use number and anphabet only is the better way */
+   const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+   const newstring = string
+                     .toLowerCase()
+                     .split("")
+                     .filter(character => {
+                        return alphanumerical.includes(character);
+                     })
+                     .join("");
+   const reversedString = newstring.split('').reverse().join('');
+   return newstring === reversedString;
 };
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
