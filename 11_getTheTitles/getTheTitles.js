3a55e86435376a32eb1145824bdd4a6aadb0d159
Dungnope <140576667+Dungnope@users.noreply.github.com>
2025-04-23 15:29:05 +0700

    Use reduce to solve this problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((acc, curr, index) =>{
+        acc[index] = curr.title;
+        return acc;
+    }, [])
 };
 
 // Do not edit below this line
